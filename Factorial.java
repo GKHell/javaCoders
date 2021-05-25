ca299d7a7f90f4ef65c7411b5222f716290d7367
GK Sir <hellofcode@hell.you>
2021-05-24 16:53:33 -1100

    KeepCoding :)

--- Factorial.java
+++ Factorial.java
@@ -12,10 +12,14 @@
 
  	int fact(int n)
  	{
+ 		System.out.println(n+" :  ");
+
  		if(n==1)
  			return 1;
- 		return n*fact(n-1);
+ 		int ans = n*fact(n-1);
+ 		System.out.println(" :  "+ans);
+ 		return ans;
  	}
  }
- // 5! = 5x4!
+ // 5! = 5x4x3x2x1
  // 4! = 4x3!
