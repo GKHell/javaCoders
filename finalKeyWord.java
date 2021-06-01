c9a3fb7a4f31df2d3e77d103afc80b01bd2873d1
GK Sir <hellofcode@hell.you>
2021-05-31 16:16:21 -1100

    find the new way

---
+++ finalKeyWord.java
@@ -0,0 +1,23 @@
+class polyWithfinal{
+	public static void main(String[] args) {
+		
+		B b = new B();
+		b.say2();
+	}
+}
+final class A{
+
+	final void say1()
+	{
+		System.out.println("we are in Class A");
+	}
+}
+class B extends A{
+
+	void say2()
+	{
+		say1();
+		System.out.println("we are in Class B");
+
+	}
+}
