dc0742892750c8314f7ada6d0c8835037865e055
GK Sir <hellofcode@hell.you>
2021-05-31 16:46:05 -1100

    find the new way

--- typeCast.java
+++ typeCast.java
@@ -16,5 +16,37 @@ class typeCast{
 		int y = (int)x;
 		System.out.println("y : "+y);
 
+	/////////////////////////////
+
+
+
+		
+		A a = new B();
+		B b = (B)a; // specilazation  //super -> child
+
+		b.say1();
+		b.say2();
+
+		//////////////////////////
+		A objA = new B();// child to super
+		objA.say1();
+
+	}
+}
+
+class A
+{
+	void say1()
+	{
+		System.out.println("hello A");
+	}
+}
+
+
+class B extends A
+{
+	void say2()
+	{
+		System.out.println("hello B");
 	}
 }
