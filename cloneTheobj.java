502ac17d1f47c605208a0a1c86cbafcc59666ee1
GK Sir <hellofcode@hell.you>
2021-06-01 16:17:15 +0530

    Tue01Jun202104:17:15PM+0530

---
+++ cloneTheobj.java
@@ -0,0 +1,23 @@
+class cloneTheobj{
+	public static void main(String[] args) throws CloneNotSupportedException {
+		
+		Code c1 = new Code();
+
+		Code c2 = (Code)c1.clone();
+
+		System.out.println("c1 : "+c1.i);
+			 		
+		System.out.println("c2 : "+c2.i);
+
+	}
+}
+class Code implements Cloneable{
+	
+	int i = 300;
+
+	protected Object clone() throws CloneNotSupportedException
+	{
+		return super.clone();
+	}
+
+}
