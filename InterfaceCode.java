9aa1ca598660c00c013fa0243d491e18153203f2
GK Sir <hellofcode@hell.you>
2021-06-02 16:16:52 +0530

    Wed02Jun202104:16:52PM+0530

---
+++ InterfaceCode.java
@@ -0,0 +1,17 @@
+
+class InterfaceCode{ 
+	public static void main(String[] args) {
+	
+		Code c = new Code();	
+	}
+
+}
+interface Rules{
+
+	void display(); //abstract method...
+
+}
+class Code{
+
+
+}
