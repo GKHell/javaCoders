5fec6fa1855850d8f2927d950dab0a1ced4b1b9b
GK Sir <hellofcode@hell.you>
2021-06-14 16:55:43 +0530

    Mon14Jun202104:55:43PM+0530

---
+++ arrayCode.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+class Code{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the size : ");
+		int n = sc.nextInt();
+
+		int a[] = new int[n];
+		System.out.println("Enter the array : \n");
+		for(int i =0;i<n;i++)
+		{
+			System.out.print("a[ "+i+" ] = ");
+			a[i] = sc.nextInt();
+		}
+
+		System.out.println("\n-----------------------\n");
+		// x = a;
+		for(int x:a) //for-each
+		{
+			System.out.println("==> : "+x);
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("----------- : "+a[i]);
+		}
+	}
+}
